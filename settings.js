84d61dd2eb330ace8de64c60f188b89451f72ab0
OzaProject <99927121+Sasaprojectb@users.noreply.github.com>
2023-04-20 07:11:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Fahroza'// yourname
 global.MONGO_DB_URI = "mongodb+srv://Fahrozaa:Alifcahpasoan12#@custer1.o7grfuh.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "fahrozabot.verifikasi@gmail.com" //email
+global.email_password = "dphpflgwdcbwmxhb" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
